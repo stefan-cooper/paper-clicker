e763d6d3992ebb45dd1c6cb4bc1ec9a0273f0f6f
Stefan Cooper <stefan.cooper27@gmail.com>
2020-06-09 19:07:09 +0100

    Updates

---
+++ precache-manifest.db9e947374553eef0a42a17dc580a60a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "328f4fe6ae44a792b119a8f36ad35d2f",
+    "url": "/clicker-game/index.html"
+  },
+  {
+    "revision": "9ef1c08b208687a8148d",
+    "url": "/clicker-game/static/css/main.a3353276.chunk.css"
+  },
+  {
+    "revision": "1c14eaca2e9eae4423e5",
+    "url": "/clicker-game/static/js/2.88555788.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/clicker-game/static/js/2.88555788.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ef1c08b208687a8148d",
+    "url": "/clicker-game/static/js/main.fdc5fcef.chunk.js"
+  },
+  {
+    "revision": "e17d463282c5143d2a3e",
+    "url": "/clicker-game/static/js/runtime-main.853da896.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicker-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
