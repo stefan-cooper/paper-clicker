296635ca29160673251f88f190c8bf0e6b73896d
Stefan Cooper <stefan.cooper27@gmail.com>
2021-05-12 23:56:52 +0100

    Updates

---
+++ precache-manifest.63ea406dce138985e55e3c174c3b51f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e3ff4488c46d0617584f4bc79dddf6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ea83a177b65ab3bf2e1",
+    "url": "/static/css/main.072bb0a4.chunk.css"
+  },
+  {
+    "revision": "c642cf3c7c155527d838",
+    "url": "/static/js/2.ce23c95d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ce23c95d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ea83a177b65ab3bf2e1",
+    "url": "/static/js/main.cb990435.chunk.js"
+  },
+  {
+    "revision": "0ab6d7e33ed0a54af35a",
+    "url": "/static/js/runtime-main.2cd37260.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/paper-clicker/precache-manifest.d6ac7fb71480c745e3f0d5ac1ef0d434.js"
+  "/precache-manifest.63ea406dce138985e55e3c174c3b51f6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/paper-clicker/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
